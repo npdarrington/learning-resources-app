3c0df63d15150fcc02addaa5dbd4b4a710086afa
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 16:56:31 -0700

    Add storedResouces to TheResources

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -22,6 +22,20 @@ export default {
 	data() {
 		return {
 			selectedTab: 'stored-resources',
+			storedResources: [
+				{
+					id: 'official-guide',
+					title: 'Official Guide',
+					description: 'The official Vue.js documentation',
+					link: 'https://vuejs.org',
+				},
+				{
+					id: 'google',
+					title: 'Google',
+					description: 'Learn to Google',
+					link: 'https://google.com',
+				},
+			],
 		};
 	},
 	methods: {
