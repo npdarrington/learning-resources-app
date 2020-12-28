5217dea2bae4877afb2de12b833c586c4bf8263e
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 10:38:49 -0700

    Interpolate and bind props to template

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -2,13 +2,13 @@
 	<li>
 		<article>
 			<header>
-				<h3></h3>
+				<h3>{{ title }}</h3>
 				<button>Delete</button>
 			</header>
 		</article>
-		<p></p>
+		<p>{{ description }}</p>
 		<nav>
-			<a href="#">View Resource</a>
+			<a :href="link">View Resource</a>
 		</nav>
 	</li>
 </template>
