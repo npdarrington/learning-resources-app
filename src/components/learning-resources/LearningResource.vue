165a74546b63d5b07e0a29a832327221b7400a0e
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 10:34:15 -0700

    Setup initial HTML structure for LearningResource

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,14 @@
+<template>
+	<li>
+		<article>
+			<header>
+				<h3></h3>
+				<button>Delete</button>
+			</header>
+		</article>
+		<p></p>
+		<nav>
+			<a href="#">View Resource</a>
+		</nav>
+	</li>
+</template>
