22836a01053a8fce8893d3e06ca82083b2aafd8b
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 16:07:52 -0700

    Remove ul from App to StoredResources

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,4 @@
-<template>
-	<ul>
-		<learning-resource
-			v-for="resource in storedResources"
-			:key="resource.id"
-			:title="resource.title"
-			:description="resource.description"
-			:link="resource.link"
-		></learning-resource>
-	</ul>
-</template>
+<template> </template>
 
 <script>
 import LearningResource from './components/learning-resources/LearningResource';
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,11 @@
+<template>
+	<ul>
+		<learning-resource
+			v-for="resource in storedResources"
+			:key="resource.id"
+			:title="resource.title"
+			:description="resource.description"
+			:link="resource.link"
+		></learning-resource>
+	</ul>
+</template>
