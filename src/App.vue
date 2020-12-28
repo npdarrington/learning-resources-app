f11f5a6e2592f2c67b21bc5fc17ff384a1d111fc
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 10:39:53 -0700

    Pass props to LearningResource

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
 		<learning-resource
 			v-for="resource in storedResources"
 			:key="resource.id"
+			:title="resource.title"
+			:description="resource.description"
+			:link="resource.link"
 		></learning-resource>
 	</ul>
 </template>
