e602508385cbca9d638dc5eb9b635fe0a0d732a8
Nathan Darrington <npdarrington@icloud.com>
2020-12-28 17:16:56 -0700

    Added form for AddResource with title description and link fields

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,21 @@
 <template>
-	<h2>Add Resource</h2>
+	<base-card>
+		<form>
+			<div class="form-control">
+				<label for="title">Title:</label>
+				<input type="text" name="title" id="title" />
+			</div>
+			<div class="form-control">
+				<label for="title">Description:</label>
+				<textarea id="description" name="description" rows="3"></textarea>
+			</div>
+			<div class="form-control">
+				<label for="link">Link:</label>
+				<input type="url" name="link" id="link" />
+			</div>
+			<div>
+				<base-button type="Submit">Add Resource</base-button>
+			</div>
+		</form>
+	</base-card>
 </template>
